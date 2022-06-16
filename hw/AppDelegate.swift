aa9eab337af7088351ee1f176deddea8dcdc61f1
qwertyyb <qwertyyb@qq.com>
2022-06-16 22:41:14 +0800

    Initial Commit

---
+++ hw/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  hw
+//
+//  Created by 虚幻 on 2022/6/16.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ hw/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hw
+//
+//  Created by 虚幻 on 2022/6/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
